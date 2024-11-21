c518f9a3f7c56543b90db487c029fe0dc8fffa40
Lowky51 <allanfleaux3@gmail.com>
2024-11-21 15:26:01 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ const burger = document.getElementById('burger');
         burger.addEventListener('click', () => {
             navbarMenu.classList.toggle('active');
         });
+
+
+        // SCROLL Profil
+
+        document.getElementById('scrollProfil').addEventListener('click', function() {
+            document.getElementById('profil').scrollIntoView({ behavior: 'smooth' });
+        });
+                
+
+        // SCROLL COMPETENCES
+        document.getElementById('scrollCompetence').addEventListener('click', function() {
+            document.getElementById('competence').scrollIntoView({ behavior: 'smooth' });
+        });
+
+        // SCROLL PROJET
+        document.getElementById('scrollProjet').addEventListener('click', function() {
+            document.getElementById('projet').scrollIntoView({ behavior: 'smooth' });
+        });
+                
