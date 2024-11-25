3b01be54c2dc8eaf0fe0a855338d933a0d10efd5
Lowky51 <allanfleaux3@gmail.com>
2024-11-25 15:47:10 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -93,3 +93,27 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     document.getElementById('en-savoir-plus_3').style.display = 'none'
   }
 
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const elements = document.querySelectorAll('.fade-in');
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.1 // Ajustez ce seuil selon vos besoins
+    });
+
+    elements.forEach(element => {
+        observer.observe(element);
+    });
+});
+
+
+
